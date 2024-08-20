ca0678b91f10093304264e46009cbfc46973d669
Rafael Rodrigues <rafael.rodrigues.rr1503@gmail.com>
2024-08-20 03:03:27 -0300

    Added functionality for downloading links on a text file

--- yt_download.py
+++ yt_download.py
@@ -13,32 +13,41 @@ settings = {
         'preferredquality': '320'
     }],
     'outtmpl': '/%(title)s.%(ext)s',
-    'ffmpeg_location': os.path.join(os.getcwd(), 'ffmpeg.exe')
+    'ffmpeg_location': "D:\\Code\\ytDownload\\ffmpeg.exe"
 }
 dlfolder = str(Path.home() / "Downloads")
 
-# Get links from user input
+
 links = []
 playlists = []
-print('Enter YouTube URLs:')
-n = 1
-while True:
-    link = input(f'{n}. ')
-    if not link:
-        break
+print("Get links from text file? (y/N)")
+if input().lower() == 'y':
+    # Get links from text file
+    with open('links.txt', 'r') as f:
+        for line in f.read().split('\n'):
+            links.append(line)
 
-    if '&list=' in link:
-        video_url = link.split("&list=")[0]
-        playlist = link.split("&list=")[1].split("&index=")[0]
+else:
+    # Get links from user input
+    print('Enter YouTube URLs:')
+    n = 1
+    while True:
+        link = input(f'{n}. ')
+        if not link:
+            break
+        links.append(link)
+        n += 1
 
-        print(f"Download playlist {playlist}? (Y/n)")
+for li in links:
+    if '&list=' in li:
+        video_url = li.split("&list=")[0]
+        playlist_id = li.split("&list=")[1].split("&index=")[0]
+
+        print(f"Download playlist {playlist_id}? (Y/n)")
         if input().lower() == 'n':
             links.append(video_url)
-            n += 1
             continue
 
-    links.append(link)
-    n += 1
 
 # Download the songs
 os.chdir(dlfolder)
@@ -49,6 +58,7 @@ for link in links:
             ydl.download([link])
         except yt_dlp.DownloadError:
             print(f"Couldn't download {link}")
+
 
 # Rename .mp3 files
 for filename in glob.glob('*.mp3'):
